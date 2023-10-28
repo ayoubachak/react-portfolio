33ac7fbdf106dff35b67da08071f3cb2a175b0ca
Ayoub Achak <ayoub.achak01@gmail.com>
2023-10-29 00:55:16 +0200

    modified and reduced the Blob generation

--- src/components/canvas/Blob.jsx
+++ src/components/canvas/Blob.jsx
@@ -90,11 +90,11 @@ const Blob = ({ position, scale, geometryArgs }) => {
 const BlobCanvas = () => {
     const blobs = useMemo(() => {
       const temp = [];
-      for (let i = 0; i < 20; i++) {
-        const x = MathUtils.randFloat(10, 30);  // Generate only positive x
-        const y = MathUtils.randFloatSpread(30);
-        const z = MathUtils.randFloatSpread(30);
-        const scale = MathUtils.randFloat(0.5, 2);
+      for (let i = 0; i < 5; i++) {
+        const x = MathUtils.randFloat(-30, 30);  // Generate only positive x
+        const y = MathUtils.randFloat(-20,0);
+        const z = MathUtils.randFloat(0,30);
+        const scale = MathUtils.randFloat(1, 2);
         const geometryArgs = [MathUtils.randFloat(1, 3), 20];
         temp.push(
           <Blob
